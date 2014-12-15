20a17dcda5269e3724fabf476bad23c4504185a1
zubiri <zubiri@Ubuntu-12-04.(none)>
2014-12-15 14:24:19 +0100

    ordenar array

---
+++ src/Ejercicio1.java
@@ -0,0 +1,26 @@
+public class Ejercicio1 {
+	public static void main(String[] args) {
+		int [] arrayNumeros = {2,5,6,9,1,0};
+
+		int aux;
+ 		System.out.println("Array sin ordenar:");
+	    for (int i=0; i<arrayNumeros.length; i++) {
+	    	System.out.print(arrayNumeros[i]+" ");
+	    }
+
+	    // ordenar elementos del array
+	    for (int i=0; i<arrayNumeros.length; i++) {
+	        for (int j=i+1; j<arrayNumeros.length; j++) {
+	            if (arrayNumeros[j] < arrayNumeros[i]) {
+	                aux = arrayNumeros[i];
+	                arrayNumeros[i] = arrayNumeros[j];
+	                arrayNumeros[j] = aux;
+	            }
+	        }
+	    }
+	    System.out.println("\nArray ordenado:");
+	    for (int i=0; i<arrayNumeros.length; i++) {
+	    	System.out.print(arrayNumeros[i]+" ");
+	    }
+	}
+}
